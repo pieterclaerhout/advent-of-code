b6bf8a568d36d418528e294373b392d4d9d7deac
Pieter Claerhout <pieter@contractify.be>
2022-12-22 17:56:51 +0100

    Day 6

---
+++ day06/day06.ts
@@ -0,0 +1,40 @@
+import { readFileSync } from "../utils/readfile.ts";
+
+const parseInput = (path: string): string[] => {
+  return readFileSync(path).split('');
+}
+
+
+const findUniqueCharsPosition = (chars: string[], setSize: number) => {
+	const lastChars: string[] = [];
+	for (let i = 0; i < chars.length; i++) {
+		lastChars.push(chars[i]);
+		if (lastChars.length > setSize) {
+      lastChars.shift();
+    }
+		if (new Set(lastChars).size === setSize) {
+			return i + 1
+		}
+	}
+	throw 'failed'
+}
+
+const part1 = (parsedInput: string[]) => {
+  const result = findUniqueCharsPosition(parsedInput, 4);
+  console.log("Part 1", result)
+}
+
+const part2 = (parsedInput: string[]) => {
+  const result = findUniqueCharsPosition(parsedInput, 14);
+  console.log("Part 2", result);
+}
+
+const run = () => {
+  const inputPath = new URL('input.txt', import.meta.url).pathname;
+  const parsedInput = parseInput(inputPath);
+
+  part1(parsedInput);
+  part2(parsedInput);
+}
+
+export default run;
