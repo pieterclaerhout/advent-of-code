4e9175a4d458a02eebc5182d72bd3c5599c55b79
Pieter Claerhout <pieter@contractify.be>
2022-12-23 15:23:50 +0100

    Day 23 v3

--- day23/day23.ts
+++ day23/day23.ts
@@ -1,229 +1,326 @@
 import { readFileSync } from "../utils/readfile.ts";
 
-const DEBUG = false;
-
-export type It<T> = IterableIterator<T>;
-
-function* indexed<T>(iter: Iterable<T>): It<[T, number]> {
-  let i = 0;
-  for (const elem of iter) {
-    yield [elem, i++];
-  }
-}
-
-function* range(from: number, to: number, step = 1): It<number> {
-  if (to >= from) {
-    for (let i = from; i <= to; i += step) {
-      yield i;
-    }
-  } else {
-    for (let i = from; i >= to; i -= step) {
-      yield i;
-    }
-  }
-}
-
-export const countIters = (pred: () => boolean): number => {
-  let count = 0;
-
-  while (pred()) {
-    count++;
-  }
-
-  return count;
+type Pos = {
+  x: number;
+  y: number;
 };
 
-type Dir = "N" | "S" | "E" | "W" | "NE" | "NW" | "SE" | "SW";
-type Pos = { x: number; y: number };
-type PosHash = number;
+const North = [{ x: -1, y: -1 }, { x: 0, y: -1 }, { x: 1, y: -1 }];
+const South = [{ x: -1, y: 1 }, { x: 0, y: 1 }, { x: 1, y: 1 }];
+const East = [{ x: 1, y: -1 }, { x: 1, y: 0 }, { x: 1, y: 1 }];
+const West = [{ x: -1, y: -1 }, { x: -1, y: 0 }, { x: -1, y: 1 }];
 
-const K = 1000;
-const OFFSET = { x: K / 2, y: K / 2 };
+// const DEBUG = false;
 
-const hashPos = (x: number, y: number): PosHash =>
-  (OFFSET.x + x) * K + (OFFSET.y + y);
+// export type It<T> = IterableIterator<T>;
 
-const parsePos = (pos: PosHash): Pos => {
-  const x = Math.floor(pos / K) - OFFSET.x;
-  const y = pos - (x + OFFSET.x) * K - OFFSET.y;
-  return { x, y };
-};
+// function* indexed<T>(iter: Iterable<T>): It<[T, number]> {
+//   let i = 0;
+//   for (const elem of iter) {
+//     yield [elem, i++];
+//   }
+// }
 
-type ElfMap = Set<PosHash>;
+// function* range(from: number, to: number, step = 1): It<number> {
+//   if (to >= from) {
+//     for (let i = from; i <= to; i += step) {
+//       yield i;
+//     }
+//   } else {
+//     for (let i = from; i >= to; i -= step) {
+//       yield i;
+//     }
+//   }
+// }
 
-const parseInput = (path: string): ElfMap => {
-  const lines = readFileSync(path)
-    .split("\n");
+// export const countIters = (pred: () => boolean): number => {
+//   let count = 0;
 
-  const map = new Set<PosHash>();
+//   while (pred()) {
+//     count++;
+//   }
 
-  for (const [line, y] of indexed(lines)) {
-    for (const [char, x] of indexed(line)) {
-      if (char === "#") {
-        map.add(hashPos(x, y));
-      }
-    }
-  }
+//   return count;
+// };
 
-  return map;
-};
+// type Dir = "N" | "S" | "E" | "W" | "NE" | "NW" | "SE" | "SW";
+// type Pos = { x: number; y: number };
+// type PosHash = number;
 
-const DIR_DELTA: Record<Dir, { x: number; y: number }> = {
-  N: { x: 0, y: -1 },
-  S: { x: 0, y: 1 },
-  E: { x: 1, y: 0 },
-  W: { x: -1, y: 0 },
-  NE: { x: 1, y: -1 },
-  NW: { x: -1, y: -1 },
-  SE: { x: 1, y: 1 },
-  SW: { x: -1, y: 1 },
-};
+// const K = 1000;
+// const OFFSET = { x: K / 2, y: K / 2 };
 
-const createDirections = () => {
-  const directions: { ifFree: Dir[]; go: Dir }[] = [
-    { ifFree: ["N", "NE", "NW"], go: "N" },
-    { ifFree: ["S", "SE", "SW"], go: "S" },
-    { ifFree: ["W", "NW", "SW"], go: "W" },
-    { ifFree: ["E", "NE", "SE"], go: "E" },
-  ];
+// const hashPos = (x: number, y: number): PosHash =>
+//   (OFFSET.x + x) * K + (OFFSET.y + y);
 
-  const rotate = () => {
-    const first = directions.shift()!;
-    directions.push(first);
-  };
+// const parsePos = (pos: PosHash): Pos => {
+//   const x = Math.floor(pos / K) - OFFSET.x;
+//   const y = pos - (x + OFFSET.x) * K - OFFSET.y;
+//   return { x, y };
+// };
 
-  return { directions, rotate };
-};
+// type ElfMap = Set<PosHash>;
 
-type RotatedDirections = ReturnType<typeof createDirections>;
+// const parseInput = (path: string): ElfMap => {
+//   const lines = readFileSync(path)
+//     .split("\n");
 
-const ALL_DIRECTIONS: Dir[] = ["N", "S", "E", "W", "NE", "NW", "SE", "SW"];
+//   const map = new Set<PosHash>();
 
-const proposeMoves = (elves: ElfMap, dirs: RotatedDirections) => {
-  const propositionCounts = new Map<PosHash, number>();
-  const propositions = new Map<PosHash, PosHash>();
+//   for (const [line, y] of indexed(lines)) {
+//     for (const [char, x] of indexed(line)) {
+//       if (char === "#") {
+//         map.add(hashPos(x, y));
+//       }
+//     }
+//   }
 
-  for (const elf of elves) {
-    const { x, y } = parsePos(elf);
+//   return map;
+// };
 
-    for (const { ifFree, go } of dirs.directions) {
-      const completelyFree = ALL_DIRECTIONS.every((dir) => {
-        const { x: dx, y: dy } = DIR_DELTA[dir];
-        return !elves.has(hashPos(x + dx, y + dy));
-      });
+// const DIR_DELTA: Record<Dir, { x: number; y: number }> = {
+//   N: { x: 0, y: -1 },
+//   S: { x: 0, y: 1 },
+//   E: { x: 1, y: 0 },
+//   W: { x: -1, y: 0 },
+//   NE: { x: 1, y: -1 },
+//   NW: { x: -1, y: -1 },
+//   SE: { x: 1, y: 1 },
+//   SW: { x: -1, y: 1 },
+// };
 
-      if (!completelyFree) {
-        if (
-          ifFree.every((dir) => {
-            const { x: dx, y: dy } = DIR_DELTA[dir];
-            return !elves.has(hashPos(x + dx, y + dy));
-          })
-        ) {
-          const delta = DIR_DELTA[go];
-          const key = hashPos(x + delta.x, y + delta.y);
-          propositionCounts.set(key, (propositionCounts.get(key) ?? 0) + 1);
-          propositions.set(elf, key);
-          break;
-        }
-      }
-    }
-  }
+// const createDirections = () => {
+//   const directions: { ifFree: Dir[]; go: Dir }[] = [
+//     { ifFree: ["N", "NE", "NW"], go: "N" },
+//     { ifFree: ["S", "SE", "SW"], go: "S" },
+//     { ifFree: ["W", "NW", "SW"], go: "W" },
+//     { ifFree: ["E", "NE", "SE"], go: "E" },
+//   ];
 
-  return { propositions, propositionCounts };
-};
+//   const rotate = () => {
+//     const first = directions.shift()!;
+//     directions.push(first);
+//   };
 
-const move = (
-  propositions: Map<PosHash, PosHash>,
-  propositionCounts: Map<PosHash, number>,
-  elves: ElfMap,
-): boolean => {
-  let moved = false;
+//   return { directions, rotate };
+// };
 
-  for (const [from, to] of propositions) {
-    if (propositionCounts.get(to)! < 2) {
-      elves.delete(from);
-      elves.add(to);
-      moved = true;
-    }
-  }
+// type RotatedDirections = ReturnType<typeof createDirections>;
 
-  return moved;
-};
+// const ALL_DIRECTIONS: Dir[] = ["N", "S", "E", "W", "NE", "NW", "SE", "SW"];
 
-const round = (elves: ElfMap, dirs: RotatedDirections): boolean => {
-  const { propositions, propositionCounts } = proposeMoves(elves, dirs);
-  const moved = move(propositions, propositionCounts, elves);
-  dirs.rotate();
-  return moved;
-};
+// const proposeMoves = (elves: ElfMap, dirs: RotatedDirections) => {
+//   const propositionCounts = new Map<PosHash, number>();
+//   const propositions = new Map<PosHash, PosHash>();
 
-type Bounds = { minX: number; maxX: number; minY: number; maxY: number };
+//   for (const elf of elves) {
+//     const { x, y } = parsePos(elf);
 
-const bounds = (elves: ElfMap): Bounds => {
-  let minX = Infinity;
-  let maxX = -Infinity;
-  let minY = Infinity;
-  let maxY = -Infinity;
+//     for (const { ifFree, go } of dirs.directions) {
+//       const completelyFree = ALL_DIRECTIONS.every((dir) => {
+//         const { x: dx, y: dy } = DIR_DELTA[dir];
+//         return !elves.has(hashPos(x + dx, y + dy));
+//       });
 
-  for (const elf of elves) {
-    const { x, y } = parsePos(elf);
+//       if (!completelyFree) {
+//         if (
+//           ifFree.every((dir) => {
+//             const { x: dx, y: dy } = DIR_DELTA[dir];
+//             return !elves.has(hashPos(x + dx, y + dy));
+//           })
+//         ) {
+//           const delta = DIR_DELTA[go];
+//           const key = hashPos(x + delta.x, y + delta.y);
+//           propositionCounts.set(key, (propositionCounts.get(key) ?? 0) + 1);
+//           propositions.set(elf, key);
+//           break;
+//         }
+//       }
+//     }
+//   }
 
-    minX = Math.min(minX, x);
-    maxX = Math.max(maxX, x);
-    minY = Math.min(minY, y);
-    maxY = Math.max(maxY, y);
-  }
+//   return { propositions, propositionCounts };
+// };
 
-  return { minX, maxX, minY, maxY };
-};
+// const move = (
+//   propositions: Map<PosHash, PosHash>,
+//   propositionCounts: Map<PosHash, number>,
+//   elves: ElfMap,
+// ): boolean => {
+//   let moved = false;
 
-const show = (elves: ElfMap) => {
-  const { minX, maxX, minY, maxY } = bounds(elves);
-  const lines: string[] = [];
+//   for (const [from, to] of propositions) {
+//     if (propositionCounts.get(to)! < 2) {
+//       elves.delete(from);
+//       elves.add(to);
+//       moved = true;
+//     }
+//   }
 
-  for (const y of range(minY, maxY + 1)) {
-    let line = "";
-    for (const x of range(minX, maxX + 1)) {
-      line += elves.has(hashPos(x, y)) ? "#" : ".";
-    }
-    lines.push(line);
-  }
+//   return moved;
+// };
 
-  return lines.join("\n");
-};
+// const round = (elves: ElfMap, dirs: RotatedDirections): boolean => {
+//   const { propositions, propositionCounts } = proposeMoves(elves, dirs);
+//   const moved = move(propositions, propositionCounts, elves);
+//   dirs.rotate();
+//   return moved;
+// };
 
-const countEmptyTiles = (elves: ElfMap): number => {
-  const { minX, maxX, minY, maxY } = bounds(elves);
-  const area = (maxX - minX + 1) * (maxY - minY + 1);
-  return area - elves.size;
-};
+// type Bounds = { minX: number; maxX: number; minY: number; maxY: number };
 
-const part1 = (inputPath: string): void => {
-  const elves = parseInput(inputPath);
-  const dirs = createDirections();
+// const bounds = (elves: ElfMap): Bounds => {
+//   let minX = Infinity;
+//   let maxX = -Infinity;
+//   let minY = Infinity;
+//   let maxY = -Infinity;
 
-  for (const _ of range(1, 10)) {
-    round(elves, dirs);
-  }
+//   for (const elf of elves) {
+//     const { x, y } = parsePos(elf);
 
-  const result = countEmptyTiles(elves);
+//     minX = Math.min(minX, x);
+//     maxX = Math.max(maxX, x);
+//     minY = Math.min(minY, y);
+//     maxY = Math.max(maxY, y);
+//   }
 
-  console.log("Part 1:", result);
-};
+//   return { minX, maxX, minY, maxY };
+// };
 
-const part2 = (inputPath: string): void => {
-  const elves = parseInput(inputPath);
-  const dirs = createDirections();
-  const result = countIters(() => round(elves, dirs)) + 1;
+// const show = (elves: ElfMap) => {
+//   const { minX, maxX, minY, maxY } = bounds(elves);
+//   const lines: string[] = [];
 
-  console.log("Part 2:", result);
-};
+//   for (const y of range(minY, maxY + 1)) {
+//     let line = "";
+//     for (const x of range(minX, maxX + 1)) {
+//       line += elves.has(hashPos(x, y)) ? "#" : ".";
+//     }
+//     lines.push(line);
+//   }
+
+//   return lines.join("\n");
+// };
+
+// const countEmptyTiles = (elves: ElfMap): number => {
+//   const { minX, maxX, minY, maxY } = bounds(elves);
+//   const area = (maxX - minX + 1) * (maxY - minY + 1);
+//   return area - elves.size;
+// };
+
+// const part1 = (inputPath: string): void => {
+//   const elves = parseInput(inputPath);
+//   const dirs = createDirections();
+
+//   for (const _ of range(1, 10)) {
+//     round(elves, dirs);
+//   }
+
+//   const result = countEmptyTiles(elves);
+
+//   console.log("Part 1:", result);
+// };
+
+// const part2 = (inputPath: string): void => {
+//   const elves = parseInput(inputPath);
+//   const dirs = createDirections();
+//   const result = countIters(() => round(elves, dirs)) + 1;
+
+//   console.log("Part 2:", result);
+// };
 
 const run = () => {
   const inputPath = new URL("input.txt", import.meta.url).pathname;
 
-  part1(inputPath);
-  part2(inputPath);
+  let moves = [North, South, West, East];
+
+  let garden = new Set<string>();
+  let elves: Pos[] = [];
+
+  function str(pos: Pos): string {
+    return pos.x + ";" + pos.y;
+  }
+
+  function get_move(elve: Pos): Pos | void {
+    let count = 0;
+    let pos = elve;
+
+    for (let move of moves) {
+      if (
+        move.every((e) => {
+          return !garden.has(str({ x: elve.x + e.x, y: elve.y + e.y }));
+        })
+      ) {
+        if (count == 0) {
+          pos = { x: elve.x + move[1].x, y: elve.y + move[1].y };
+        }
+        count++;
+      }
+    }
+
+    if (count < 4 && count != 0) {
+      return pos;
+    }
+  }
+
+  readFileSync(inputPath)
+    .split("\n")
+    .forEach((line, y) => {
+      line.split("")
+        .forEach((char, x) => {
+          if (char == "#") {
+            garden.add(str({ x, y }));
+            elves.push({ x, y });
+          }
+        });
+    });
+
+  for (let i = 1; i < Number.MAX_SAFE_INTEGER; i++) {
+    let proposed_moves: [number, Pos][] = [];
+
+    elves.forEach((elve, id) => {
+      let proposal = get_move(elve);
+      if (proposal) {
+        proposed_moves.push([id, proposal]);
+      }
+    });
+
+    proposed_moves.forEach((move) => {
+      if (
+        proposed_moves.filter((e) => {
+          return e[1].x == move[1].x && e[1].y == move[1].y;
+        }).length == 1
+      ) {
+        garden.delete(str(elves[move[0]]));
+        elves[move[0]] = move[1];
+        garden.add(str(move[1]));
+      }
+    });
+
+    moves.push(moves.shift()!);
+
+    if (i == 10) { // part 1
+      let maxx = elves[0].x;
+      let minx = maxx;
+      let maxy = elves[0].y;
+      let miny = maxy;
+      for (let elve of elves) {
+        if (elve.x > maxx) maxx = elve.x;
+        if (elve.x < minx) minx = elve.x;
+        if (elve.y > maxy) maxy = elve.y;
+        if (elve.y < miny) miny = elve.y;
+      }
+      let empty = (maxx - minx + 1) * (maxy - miny + 1) - elves.length;
+      console.log(empty);
+    }
+
+    if (proposed_moves.length == 0) { // part 2
+      console.log(i);
+      break;
+    }
+  }
+  // part1(inputPath);
+  // part2(inputPath);
 };
 
 export default run;
