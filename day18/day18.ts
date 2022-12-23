6e90dc0c510aa61766f39cca932c4d043829dd38
Pieter Claerhout <pieter@contractify.be>
2022-12-23 13:19:41 +0100

    Day 18

---
+++ day18/day18.ts
@@ -0,0 +1,140 @@
+import { readFileSync } from "../utils/readfile.ts";
+
+const parseInput = (path: string): number[][] =>
+  readFileSync(path)
+    .split("\n")
+    .map((line) => line.split(",").map((v) => +v));
+
+const grid = (x: number, y: number, z: number) => [x, y, z].join(",");
+
+const xyz = (grid: string) => grid.split(",").map((v) => +v);
+
+const NEIGHBORS = [
+  [1, 0, 0],
+  [-1, 0, 0],
+  [0, 1, 0],
+  [0, -1, 0],
+  [0, 0, 1],
+  [0, 0, -1],
+];
+
+const neighbors = function* (x: number, y: number, z: number) {
+  for (const [nx, ny, nz] of NEIGHBORS) {
+    yield [x + nx, y + ny, z + nz];
+  }
+};
+
+const part1 = (path: string) => {
+  const input = parseInput(path);
+  const gridMap: Set<string> = new Set();
+  let surfaceArea = 0;
+  for (const [x, y, z] of input) {
+    gridMap.add(grid(x, y, z));
+    surfaceArea += 6;
+    for (const [nx, ny, nz] of neighbors(x, y, z)) {
+      const nGrid = grid(nx, ny, nz);
+      if (gridMap.has(nGrid)) {
+        surfaceArea -= 2;
+      }
+    }
+  }
+
+  console.log("Part 1:", surfaceArea);
+};
+
+const part2 = (path: string) => {
+  const input = parseInput(path);
+  const cubeMap: Set<string> = new Set();
+  const spaceMap: Set<string> = new Set();
+
+  const bounds = [
+    Infinity,
+    -Infinity,
+    Infinity,
+    -Infinity,
+    Infinity,
+    -Infinity,
+  ];
+
+  let surfaceArea = 0;
+  for (const [x, y, z] of input) {
+    if (x < bounds[0]) bounds[0] = x;
+    if (x > bounds[1]) bounds[1] = x;
+    if (y < bounds[2]) bounds[2] = y;
+    if (y > bounds[3]) bounds[3] = y;
+    if (z < bounds[4]) bounds[4] = z;
+    if (z > bounds[5]) bounds[5] = z;
+    const thisGrid = grid(x, y, z);
+    cubeMap.add(thisGrid);
+    spaceMap.delete(thisGrid);
+    surfaceArea += 6;
+    for (const [nx, ny, nz] of neighbors(x, y, z)) {
+      const nGrid = grid(nx, ny, nz);
+      if (cubeMap.has(nGrid)) {
+        surfaceArea -= 2;
+      } else {
+        spaceMap.add(nGrid);
+      }
+    }
+  }
+
+  const allPocketCubes: Set<string> = new Set();
+  for (const space of [...spaceMap]) {
+    if (allPocketCubes.has(space)) {
+      continue;
+    }
+    const toCheck: Set<string> = new Set([space]);
+    const airCluster: Set<string> = new Set();
+    let clusterSurfaceArea = 0;
+    let enclosed = true;
+    while (enclosed && toCheck.size > 0) {
+      const [checking] = toCheck;
+      const [x, y, z] = xyz(checking);
+      airCluster.add(checking);
+      toCheck.delete(checking);
+      clusterSurfaceArea += 6;
+      for (const [nx, ny, nz] of neighbors(x, y, z)) {
+        if (
+          nx < bounds[0] ||
+          nx > bounds[1] ||
+          ny < bounds[2] ||
+          ny > bounds[3] ||
+          nz < bounds[4] ||
+          nz > bounds[5]
+        ) {
+          enclosed = false;
+          break;
+        }
+        const nGrid = grid(nx, ny, nz);
+        const cubeInCluster = airCluster.has(nGrid);
+        if (cubeInCluster) {
+          clusterSurfaceArea -= 2;
+        }
+        if (
+          !cubeMap.has(nGrid) &&
+          !cubeInCluster &&
+          !allPocketCubes.has(nGrid)
+        ) {
+          toCheck.add(nGrid);
+        }
+      }
+    }
+    if (enclosed) {
+      surfaceArea -= clusterSurfaceArea;
+      for (const space of [...airCluster]) {
+        allPocketCubes.add(space);
+      }
+    }
+  }
+
+  console.log("Part 2:", surfaceArea);
+};
+
+const run = () => {
+  const inputPath = new URL("input.txt", import.meta.url).pathname;
+
+  part1(inputPath);
+  part2(inputPath);
+};
+
+export default run;
