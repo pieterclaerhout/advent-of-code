388af0073c8fc57d7d4ee2cd52bb05e185198a26
Pieter Claerhout <pieter@contractify.be>
2022-12-23 17:42:46 +0100

    More cleanup

--- day04/day04.ts
+++ day04/day04.ts
@@ -1,41 +1,41 @@
 import { readFileSync } from "../utils/readfile.ts";
 
-type ParsedInput = [[number, number], [number, number]][];
+type Range = [number, number];
+type Pair = [Range, Range];
 
-const parseInput = (path: string): ParsedInput => {
+const parseInput = (path: string): Pair[] => {
   return readFileSync(path)
     .split("\n")
-    .map((v) =>
-      v.split(",").map((r) => r.split("-").map((s) => +s))
-    ) as ParsedInput;
+    .map((v) => v.split(",").map((r) => r.split("-").map((s) => +s)) as Pair);
 };
 
-const part1 = (parsedInput: ParsedInput) => {
-  const result = parsedInput
-    .filter((input) => {
-      const [[aStart, aEnd], [bStart, bEnd]] = input;
-      return (
-        (aStart <= bStart && aEnd >= bEnd) || (bStart <= aStart && bEnd >= aEnd)
-      );
-    })
-    .length;
+const isContained = (pair: Pair): boolean => {
+  const [left, right] = pair;
+  if (left[0] <= right[0] && right[1] <= left[1]) {
+    return true;
+  }
+  if (right[0] <= left[0] && left[1] <= right[1]) {
+    return true;
+  }
+  return false;
+};
 
+const isOverlapping = (pair: Pair): boolean => {
+  const [left, right] = pair;
+  let [min, max] = [right, left];
+  if (left[0] < right[0]) {
+    [min, max] = [left, right];
+  }
+  return min[1] >= max[0];
+};
+
+const part1 = (parsedInput: Pair[]) => {
+  const result = parsedInput.filter(isContained).length;
   console.log("Part 1:", result);
 };
 
-const part2 = (parsedInput: ParsedInput) => {
-  const result = parsedInput
-    .filter((input) => {
-      const [[aStart, aEnd], [bStart, bEnd]] = input;
-      for (let ai = aStart; ai <= aEnd; ai++) {
-        if (ai >= bStart && ai <= bEnd) {
-          return true;
-        }
-      }
-      return false;
-    })
-    .length;
-
+const part2 = (parsedInput: Pair[]) => {
+  const result = parsedInput.filter(isOverlapping).length;
   console.log("Part 2:", result);
 };
 
