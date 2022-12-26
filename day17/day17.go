f4b983105216fe1031683522275162472254931b
Pieter Claerhout <pieter@contractify.be>
2022-12-26 16:08:48 +0100

    Better day 17

--- day17/day17.go
+++ day17/day17.go
@@ -1,114 +1,180 @@
 package day17
 
 import (
-	"io"
-	"math/bits"
+	"fmt"
 )
 
 type Command struct{}
 
 func (cmd *Command) Execute(input string) (any, any) {
-	return sim(input, 2022), sim(input, 1e12)
+	return PartOne(input), PartTwo(input)
 }
 
-type shape []byte
+var shapes = [][][]int{
+	{
+		{1, 1, 1, 1},
+	},
+	{
+		{0, 1, 0},
+		{1, 1, 1},
+		{0, 1, 0},
+	},
+	{
+		{1, 1, 1},
+		{0, 0, 1},
+		{0, 0, 1},
+	},
+	{
+		{1},
+		{1},
+		{1},
+		{1},
+	},
+	{
+		{1, 1},
+		{1, 1},
+	},
+}
 
-func (s shape) coll(cb []byte, dx, dy int) bool {
-	if dx < 0 || dx > 7-s.width() {
-		return true
+type state [][]bool
+
+func (s state) FilledRows() int {
+	for y := len(s) - 1; y >= 0; y-- {
+		for _, f := range s[y] {
+			if f {
+				return y + 1
+			}
+		}
 	}
-	for i := 0; i < len(s) && dy+i < len(cb); i++ {
-		if cb[dy+i]&(s[i]>>dx) != 0 {
-			return true
+	return 0
+}
+
+func (s *state) EnsureHeight(width int, height int) {
+	for len(*s) < height {
+		*s = append(*s, make([]bool, width))
+	}
+}
+
+func (s *state) HitsShape(shape [][]int, width int, lx int, by int) bool {
+	for y, row := range shape {
+		for x, f := range row {
+			if f == 1 && (by+y < 0 || lx+x < 0 || lx+x >= width || (*s)[by+y][lx+x]) {
+				return true
+			}
 		}
 	}
 	return false
 }
 
-func (s shape) land(cb []byte, dx, dy int) []byte {
-	if len(cb) < dy+len(s) {
-		cb = append(cb, make([]byte, dy+len(s)-len(cb))...)
-	}
-	for i, b := range s {
-		cb[dy+i] |= b >> dx
-	}
-	return cb
-}
-
-func (s shape) width() (n int) {
-	for _, b := range s {
-		if n2 := 8 - bits.TrailingZeros8(b); n2 > n {
-			n = n2
+func (s *state) ApplyShape(shape [][]int, lx int, by int) {
+	for y, row := range shape {
+		for x, f := range row {
+			if f == 1 {
+				(*s)[by+y][lx+x] = true
+			}
 		}
 	}
-	return n
 }
 
-var (
-	// ####
-	shp0 shape = []byte{0b1111_0000}
+func (s *state) TryToMoveShape(shape [][]int, width int, lx int, ty int, dx int, dy int) (int, int, bool) {
+	if !s.HitsShape(shape, width, lx+dx, ty+dy) {
+		return lx + dx, ty + dy, true
+	}
+	return lx, ty, false
+}
 
-	// .#.
-	// ###
-	// .#.
-	shp1 shape = []byte{0b0100_0000, 0b1110_0000, 0b0100_0000}
+func Simulate(width int, dx int, dy int, jets string, cb func(i int, j int, state state) bool) {
+	state := state([][]bool{})
 
-	// ..#
-	// ..#
-	// ###
-	shp2 shape = []byte{0b1110_0000, 0b0010_0000, 0b0010_0000}
+	i, j := 0, 0
 
-	// #
-	// #
-	// #
-	// #
-	shp3 shape = []byte{0b1000_0000, 0b1000_0000, 0b1000_0000, 0b1000_0000}
+	for cb(i, j, state) {
+		topY := state.FilledRows()
+		shape := shapes[i%len(shapes)]
 
-	// ##
-	// ##
-	shp4 shape = []byte{0b1100_0000, 0b1100_0000}
-)
+		state.EnsureHeight(width, topY+dy+len(shape))
 
-func sim(input string, n int) int {
-	js := []byte(input)
-	cb := []byte{0b1111_1111}
-	ss := [...]shape{shp0, shp1, shp2, shp3, shp4}
-	for i, j, co := 0, 0, []int{}; i < n; i++ {
-		for dx, dy, s := 2, len(cb)+3, ss[i%len(ss)]; ; dy, j = dy-1, j+1 {
-			if s.coll(cb, dx, dy) {
-				cb = s.land(cb, dx, dy+1)
+		cx, cy := dx, topY+dy
+
+		for {
+			jet := jets[j%len(jets)]
+			j++
+
+			if jet == '<' {
+				cx, cy, _ = state.TryToMoveShape(shape, width, cx, cy, -1, 0)
+			} else {
+				cx, cy, _ = state.TryToMoveShape(shape, width, cx, cy, +1, 0)
+			}
+
+			var moved bool
+			cx, cy, moved = state.TryToMoveShape(shape, width, cx, cy, 0, -1)
+
+			if !moved {
+				state.ApplyShape(shape, cx, cy)
 				break
 			}
-			if js[j%len(js)] == '<' && !s.coll(cb, dx-1, dy) {
-				dx--
-			}
-			if js[j%len(js)] == '>' && !s.coll(cb, dx+1, dy) {
-				dx++
-			}
 		}
-		if j < len(js)*2 /* warmup */ {
-			continue
-		}
-		if len(co) == 0 /* cutoff */ {
-			co = append(co, i, j%len(js), digest(cb), len(cb))
-			continue
-		}
-		if o := i - co[0]; o%len(ss) == 0 && j%len(js) == co[1] && digest(cb) == co[2] {
-			return (n-co[0])/o*(len(cb)-co[3]) + co[(n-co[0])%o+2] - 1
-		}
-		co = append(co, len(cb))
+
+		i++
 	}
-	return len(cb) - 1
 }
 
-func digest(bs []byte) (n int) {
-	for i := 0; i < 9; i++ {
-		n = n<<7 | (int(bs[i]) & 0x7f)
-	}
-	return n
+func PartOne(insn string) int {
+	var ans int
+	Simulate(7, 2, 3, insn, func(i, j int, state state) bool {
+		if i == 2022 {
+			ans = state.FilledRows()
+			return false
+		}
+
+		return true
+	})
+
+	return ans
 }
 
-func scan(r io.Reader) (ps []byte) {
-	ps, _ = io.ReadAll(r)
-	return ps
+type memoRow struct {
+	i      int
+	height int
+}
+
+func PartTwo(insn string) int {
+	heights := make([]int, 0, 200)
+	var prefix, cycle memoRow
+
+	memo := map[string]memoRow{}
+	Simulate(7, 2, 3, insn, func(i, j int, state state) bool {
+		topFilled := state.FilledRows()
+		heights = append(heights, topFilled)
+
+		key := ""
+		for y := topFilled - 1; y >= topFilled-50; y-- {
+			if y >= 0 {
+				key += fmt.Sprintf("%v;", state[y])
+			} else {
+				key += "bedrock;"
+			}
+		}
+
+		key += fmt.Sprintf("###;%d;%d", i%len(shapes), j%len(insn))
+
+		mr, isCycle := memo[key]
+
+		if isCycle {
+			prefix = mr
+			cycle = memoRow{i - mr.i, topFilled - mr.height}
+			return false
+		}
+
+		memo[key] = memoRow{i, topFilled}
+		return true
+	})
+
+	cnt := 1000000000000
+	rep := cnt - prefix.i
+	div, rem := rep/cycle.i, rep%cycle.i
+	ans := prefix.height + div*cycle.height
+	ans += heights[len(heights)-cycle.i+rem-1] - heights[len(heights)-cycle.i-1]
+
+	return ans
 }
