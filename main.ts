204b4eebbc5110ba1a011ab652df4330fe5c0295
Pieter Claerhout <pieter@contractify.be>
2022-12-28 11:19:26 +0100

    Option to run all TypeScript solutions

--- main.ts
+++ main.ts
@@ -1,31 +1,39 @@
 #!/usr/bin/env -S deno run --allow-read
 
+const runDay = async (day: string): Promise<void> => {
+  const dayAsString = day.padStart(2, "0");
+
+  const command = (await import(
+    `./day${dayAsString}/day${dayAsString}.ts`
+  )).default;
+  if (!command) {
+    throw new Error(`Unknown day: ${day}`);
+  }
+
+  const inputPath = new URL(
+    `./day${dayAsString}/input.txt`,
+    import.meta.url,
+  ).pathname;
+
+  const rawIput = Deno.readTextFileSync(inputPath)
+    .trimEnd()
+    .replaceAll(/\r?\n/g, "\n");
+
+  command(rawIput);
+};
+
 if (import.meta.main) {
   try {
     const day = Deno.args[0];
     if (!day) {
-      throw new Error("No day specified");
+      for (let i = 1; i <= 25; i++) {
+        console.log(`>>> Day ${i} <<<`);
+        await runDay(String(i));
+        console.log();
+      }
+    } else {
+      await runDay(day);
     }
-
-    const dayAsString = String(day).padStart(2, "0");
-
-    const command = (await import(
-      `./day${dayAsString}/day${dayAsString}.ts`
-    )).default;
-    if (!command) {
-      throw new Error(`Unknown day: ${day}`);
-    }
-
-    const inputPath = new URL(
-      `./day${dayAsString}/input.txt`,
-      import.meta.url,
-    ).pathname;
-
-    const rawIput = Deno.readTextFileSync(inputPath)
-      .trimEnd()
-      .replaceAll(/\r?\n/g, "\n");
-
-    command(rawIput);
   } catch (e) {
     console.log(e);
     Deno.exit(1);
